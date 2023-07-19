056dc90325f3a5974dcbc06a04f1f07186644bb3
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-19 06:48:29 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: CK
+ * Desc: C program that prints a string using the puts function.
+ */
+
 #include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a specific string using the puts function.
+ * Return: Always 0 (SUCCESS)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
