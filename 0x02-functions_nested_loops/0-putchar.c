c46181418f65ac010b17aafd262da63be35d6225
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-19 07:40:08 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,7 @@
-/* 0-putchar.c */
+#include <stdio.h>
 
-#include <unistd.h>
-
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
-int main(void)
-{
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return (0);
+int main() {
+    printf("_putchar\n");
+    return 0;
 }
 
