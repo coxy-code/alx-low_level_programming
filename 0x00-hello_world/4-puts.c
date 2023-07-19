0f3d285c97aef14bb622a22b786f041736d55c0f
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-19 06:40:47 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,6 @@
-#include <unistd.h>
-
-/**
- * main - Entry point
- *
- * Description: Prints the quote "and that piece of art is useful" - Dora Korpar, 2015-10-19
- * to the standard error.
- *
- * Return: Always 1 (Failure)
- */
+#include <stdio.h>
 int main(void)
 {
-    const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    /* The quote to be printed */
-
-    write(2, quote, 59);
-    /* Use the write syscall to print the quote to standard error */
-
-    return (1);
-    /* Return 1 to indicate failure, as required */
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
 }
