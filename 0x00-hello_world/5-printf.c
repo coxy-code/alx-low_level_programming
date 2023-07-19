d1fd22721e2c1926f331e2a90bead0d1f508af4a
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-19 03:51:14 -0700

    hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the string "with proper grammar, but the outcome is a piece of art,"
+ *              followed by a new line using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
+
