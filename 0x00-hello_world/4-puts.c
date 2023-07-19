c51a4e313be049ff73ae9a28ebb7c010f95b1a20
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-19 06:30:06 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
  * Description: Prints "Programming is like building a multilingual puzzle,"
- *              followed by a new line using the function puts.
- *
+ *              followed by a new line using the puts function.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,");
-	return (0);
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }
