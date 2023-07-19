afcffde7043b7b74b59a12ccf72237c572b35b85
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-19 06:09:54 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,12 +10,12 @@
  */
 int main(void)
 {
-    const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    /* The quote to be printed */
+	const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	/* The quote to be printed */
 
-    write(2, quote, 59);
-    /* Use the write syscall to print the quote to standard error */
+	write(2, quote, 59);
+	/* Use the write syscall to print the quote to standard error */
 
-    return (1);
-    /* Return 1 to indicate failure, as required */
+	return (1);
+	/* Return 1 to indicate failure, as required */
 }
