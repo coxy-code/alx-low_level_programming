3f95e9f91057dd27711525e1374f43bad1cf27f3
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-19 06:06:44 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,21 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the quote "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * to the standard error.
+ *
+ * Return: Always 1 (Failure)
+ */
 int main(void)
 {
     const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    /* The quote to be printed */
+
     write(2, quote, 59);
-    return 1;
+    /* Use the write syscall to print the quote to standard error */
+
+    return (1);
+    /* Return 1 to indicate failure, as required */
 }
