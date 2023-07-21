d4e9b296a218fc5fd6742238269335f2ab5be877
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-21 04:28:33 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototype for the _putchar function */
 int _putchar(char c);
+
+/* Function prototypes for your custom functions */
+void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
