8c5cacfc8ace6bc05260d26ac3e5d73a3eed659c
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 16:25:02 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stddef.h>
 #include <stdio.h>
+#include <limits.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -16,4 +17,5 @@ char *_strcpy(char *dest, const char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
+int _atoi(char *s);
 #endif /* MAIN_H */
