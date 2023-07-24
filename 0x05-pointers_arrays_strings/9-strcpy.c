19c0aed971824d5a4e417fbd14e9e430a6f604ad
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 14:55:57 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
-* _strcpy - Copies the string pointed to by src, including the terminating null byte (\0)
-* to the buffer pointed to by dest.
+* _strcpy - Copies the string pointed to by src, including the terminating null byte (\0),
+*           to the buffer pointed to by dest.
 * @dest: Pointer to the destination buffer.
 * @src: Pointer to the source string.
+*
 * Return: The pointer to dest.
 */
 char *_strcpy(char *dest, const char *src)
 {
 char *dest_ptr = dest;
+
 while (*src != '\0')
 {
 *dest = *src;
 dest++;
 src++;
 }
+
 *dest = '\0';
-return (dest_ptr);
+
+return dest_ptr;
 }
