5bfe67e99684b9b80bde3272f24cd43f32a5f377
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 09:01:09 -0700

    functions_nested_loops

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char *str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+_puts(str);
+return (0);
+}
