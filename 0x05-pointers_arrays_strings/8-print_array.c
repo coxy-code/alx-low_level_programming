c2ecdb8768c9287b544f293005af7d0155d5cd26
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 16:55:19 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,23 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* print_array - Prints n elements of an array of integers, followed by a new line.
-* @a: Pointer to the array to be printed.
-* @n: Number of elements to be printed.
-*/
+ * print_array - Prints n elements of an array of integers.
+ * @a: The array to print.
+ * @n: The number of elements to print.
+ */
 void print_array(int *a, int n)
 {
-if (a == NULL || n <= 0)
-{
-return;
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
+        if (i < n - 1)
+            printf(", ");
+    }
+    printf("\n");
 }
 
-for (int i = 0; i < n; i++)
-{
-printf("%d", a[i]);
-if (i != n - 1)
-printf(", ");
-}
-
-printf("\n");
-}
