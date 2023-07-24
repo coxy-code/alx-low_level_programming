7ee3d4cc94e760ba725e67c709afbd8c7be1c270
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 09:51:58 -0700

    functions_nested_loops

---
+++ 0x05-pointers_arrays_strings/main_1.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char *str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+puts2(str);
+
+return (0);
+}
