514b87c2062fb7b048953f58897231723951bc7b
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 16:49:23 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,11 @@
 #include "main.h"
 
 /**
-* puts2 - Prints every other
-*  character of a string.
-* @str: The string to print. */
+* puts2 - Prints every other character of a string.
+* @str: The string to print.
+*/
 void puts2(char *str)
 {
-if (str == NULL)
-{
-_putchar('\n');
-return;
-}
-
 int i;
 
 for (i = 0; str[i] != '\0'; i += 2)
