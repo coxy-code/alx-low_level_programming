c3dabb78658a31e9d775d88667dcd2600e88abf4
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 16:12:21 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,30 +1,19 @@
 #include "main.h"
 
 /**
-* _putchar - Writes a character to the standard output (stdout).
-* @c: The character to be written.
-*
-* Return: On success, the character written is returned.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-return write(1, &c, 1);
-}
-
-/**
-* puts2 - Prints every other character of a string, starting with the first character.
-* @str: Pointer to the string to be printed.
+* puts2 - Prints every other character of a string, starting from the first.
+* @str: The input string.
 */
 void puts2(char *str)
 {
 int i = 0;
 
 while (str[i] != '\0')
-{
-_putchar(str[i]);
-i += 2; /* Move to the next even-indexed character */
+    {
+putchar(str[i]);
+i += 2;
 }
 
-_putchar('\n');
+putchar('\n');
 }
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ int _putchar(char c);
 char *_strcpy(char *dest, const char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
+void puts2(char *str);
 #endif /* MAIN_H */
