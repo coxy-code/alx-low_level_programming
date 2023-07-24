493617c0529345956fbbc663abf679b07619c2d4
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 09:48:52 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
 /**
+* _putchar - Writes a character to the standard output (stdout).
+* @c: The character to be written.
+*
+* Return: On success, the character written is returned.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return write(1, &c, 1);
+}
+
+/**
 * puts2 - Prints every other character of a string, starting with the first character.
 * @str: Pointer to the string to be printed.
 */
