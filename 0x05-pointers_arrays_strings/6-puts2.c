f882e17ef997acdace22bd0dfe4ca385c6219ca3
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 09:25:43 -0700

    functions_nested_loops

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* puts2 - Prints every other character of a string, starting with the first character.
+* @str: Pointer to the string to be printed.
+*/
+void puts2(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2; // Move to the next even-indexed character
+}
+
+_putchar('\n');
+}
