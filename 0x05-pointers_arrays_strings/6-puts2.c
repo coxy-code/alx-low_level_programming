10171b768e21688704d72c0a16d6d1e1a3f74663
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 16:38:18 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
-* puts2 - Prints every other character of a string, starting from the first.
-* @str: The input string.
-*/
+ * puts2 - Prints every other character of a string.
+ * @str: The string to print.
+ */
 void puts2(char *str)
 {
-int i = 0;
+    int i;
 
-while (str[i] != '\0')
-{
-putchar(str[i]);
-i += 2;
-}
+    for (i = 0; str[i] != '\0'; i += 2)
+        _putchar(str[i]);
 
-putchar('\n');
+    _putchar('\n');
 }
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,11 +12,11 @@ int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-int _putchar(char c);
 char *_strcpy(char *dest, const char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
 int _atoi(char *s);
 void generate_password(char *password, int length);
+
 #endif /* MAIN_H */
