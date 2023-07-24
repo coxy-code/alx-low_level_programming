dbb61c54c05dce04b0507cc29c2f867b72d34bc1
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 16:51:59 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,38 +1,20 @@
 #include "main.h"
 
 /**
-* _strlen - Returns the length of a string.
-* @str: The input string.
-*
-* Return: The length of the string.
-*/
-int _strlen(char *str)
-{
-int len = 0;
-
-while (str[len] != '\0')
-len++;
-
-return len;
-}
-
-/**
-* puts_half - Prints the second half of a string.
-* @str: The input string.
-*/
+ * puts_half - Prints the second half of a string.
+ * @str: The string to print.
+ */
 void puts_half(char *str)
 {
-int length = _strlen(str);
-int start_index;
+    int length = 0;
+    int i;
 
-if (length % 2 == 0)
-start_index = length / 2;
-else
-start_index = (length + 1) / 2;
+    while (str[length] != '\0')
+        length++;
 
-for (int i = start_index; i < length; i++)
-putchar(str[i]);
+    for (i = (length + 1) / 2; str[i] != '\0'; i++)
+        _putchar(str[i]);
 
-putchar('\n');
+    _putchar('\n');
 }
 
