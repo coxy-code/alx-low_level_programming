b90d78021124ae336dd944cbae443da7a2bc0b26
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 08:24:34 -0700

    functions_nested_loops

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n = 402;
+
+    printf("n = %d\n", n);
+    reset_to_98(&n);
+    printf("n = %d\n", n);
+
+    return (0);
+}
+
