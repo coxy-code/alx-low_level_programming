45f5b813376d72ef7fa932512e6a0f1e018738aa
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-24 17:11:40 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,41 @@
-#include "main.h"
-#include <stdlib.h>
-#include <time.h>
+	#include "main.h"
 
-/* Function to generate a random password */
-void generate_password(char *password, int length) {
-const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-int i;
+/**
+ * _atoi - Convert a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+    int result = 0;
+    int sign = 1; // Sign of the number, 1 for positive, -1 for negative
 
-for (i = 0; i < length - 1; i++) {
-password[i] = charset[rand() % (sizeof(charset) - 1)];
-}
-password[length - 1] = '\0';
-}
+    // Skip leading white spaces
+    while (*s == ' ')
+        s++;
 
-int main(void) {
-const int password_length = 10; // Set the desired length of the password
-char password[password_length];
+    // Check for the sign of the number
+    while (*s == '-' || *s == '+')
+    {
+        if (*s == '-')
+            sign *= -1;
+        s++;
+    }
 
-srand((unsigned int)time(NULL)); // Seed the random number generator with the current time
+    // Convert the remaining digits to integer
+    while (*s >= '0' && *s <= '9')
+    {
+        // Check for integer overflow
+        if (result > (INT_MAX - (*s - '0')) / 10)
+        {
+            return (sign == 1 ? INT_MAX : INT_MIN);
+        }
 
-generate_password(password, password_length);
-printf("%s\n", password);
+        result = result * 10 + (*s - '0');
+        s++;
+    }
 
-return 0;
+    return (result * sign);
 }
 
