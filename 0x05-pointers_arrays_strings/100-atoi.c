08b54a9e8d32e98fe6e905d239d5b3a04cce3570
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-25 00:16:04 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,45 +1,42 @@
-#include "main.h"
-
 /**
  * _atoi - Convert a string to an integer.
- * @s: The input string.
+ * @s: Pointer to the input string.
  *
  * Return: The converted integer.
  */
 int _atoi(char *s)
 {
-    int sign = 1;
+    int sign = 1; // 1 for positive, -1 for negative
     int result = 0;
     int i = 0;
 
-    while (s[i] != '\0')
+    // Skip leading spaces
+    while (s[i] == ' ')
+        i++;
+
+    // Check for the sign
+    while (s[i] == '-' || s[i] == '+')
     {
         if (s[i] == '-')
-            sign = -sign;
-
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            // Calculate the integer value based on the digit
-            result = result * 10 + (s[i] - '0');
-            
-            // Check for overflow
-            if (result > 0 && s[i + 1] < '0')
-            {
-                if (sign == 1)
-                    return (INT_MAX);
-                else
-                    return (INT_MIN);
-            }
-        }
-        else if (result > 0)
-        {
-            // If a non-digit character is encountered after the number,
-            // break the loop and return the current result
-            break;
-        }
+            sign *= -1;
         i++;
     }
 
-    return (result * sign);
+    // Convert numeric characters to integer
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        // Check for overflow before adding the next digit
+        if (result > (INT_MAX - (s[i] - '0')) / 10)
+        {
+            // Handle overflow by returning the maximum or minimum value of int
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+
+        // Convert the digit and add it to the result
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return result * sign;
 }
 
