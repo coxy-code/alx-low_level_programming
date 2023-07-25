7343a9289e20268a1d2ae4983ecc32a42ecba3c8
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-25 01:07:28 -0700

    functions_nested_loops

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,44 @@
 #include <limits.h>
 
 /**
- * _atoi - Convert a string to an integer.
- * @s: Pointer to the input string.
- *
- * Return: The converted integer.
- */
+* _atoi - Convert a string to an integer.
+* @s: Pointer to the input string.
+*
+* Return: The converted integer.
+*/
 int _atoi(char *s)
 {
-    int sign = 1; /* 1 for positive, -1 for negative */
-    int result = 0;
-    int i = 0;
+int sign = 1; /* 1 for positive, -1 for negative */
+int result = 0;
+int i = 0;
 
-    /* Skip leading spaces */
-    while (s[i] == ' ')
-        i++;
+/* Skip leading spaces */
+while (s[i] == ' ')
+i++;
 
-    /* Check for the sign */
-    while (s[i] == '-' || s[i] == '+')
+/* Check for the sign */
+while (s[i] == '-' || s[i] == '+')
+{
+if (s[i] == '-')
+sign *= -1;
+i++;
+}
+
+/* Convert numeric characters to integer */
+while (s[i] >= '0' && s[i] <= '9')
     {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
-
-    /* Convert numeric characters to integer */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        /* Check for overflow before adding the next digit */
-        if (result > (INT_MAX - (s[i] - '0')) / 10)
+/* Check for overflow before adding the next digit */
+if (result > (INT_MAX - (s[i] - '0')) / 10)
         {
-            /* Handle overflow by returning the maximum or minimum value of int */
-            return (sign == 1) ? INT_MAX : INT_MIN;
-        }
+/* Handle overflow by returning the maximum or minimum value of int */
+return (sign == 1) ? INT_MAX : INT_MIN;
+}
 
-        /* Convert the digit and add it to the result */
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+/* Convert the digit and add it to the result */
+result = result * 10 + (s[i] - '0');
+i++;
+}
 
-    return result * sign;
+return result * sign;
 }
 
