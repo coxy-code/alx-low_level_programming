56854b11ee06ac2ae010cb0828f6535a415faaf2
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-27 05:00:34 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: Pointer to the input string.
+ *
+ * Return: Pointer to the resulting string.
+ */
+char *rot13(char *str)
+{
+    char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+    int i, j;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (j = 0; input[j] != '\0'; j++)
+        {
+            if (str[i] == input[j])
+            {
+                str[i] = output[j];
+                break;
+            }
+        }
+    }
+
+    return str;
+}
+
