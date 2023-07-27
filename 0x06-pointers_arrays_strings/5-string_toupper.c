65c49c8721b0d1de6139183d1de57e0d7a13d4e9
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-27 04:46:15 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * string_toupper - Changes all lowercase letters of a string to uppercase.
- * @str: Pointer to the input string.
- *
- * Return: Pointer to the modified string.
- */
+* string_toupper - Changes all lowercase letters of a string to uppercase.
+* @str: Pointer to the input string.
+*
+* Return: Pointer to the modified string.
+*/
 char *string_toupper(char *str)
 {
-    char *ptr = str;
+char *ptr = str;
+while (*ptr != '\0')
+{
+if (*ptr >= 'a' && *ptr <= 'z')
+{
+*ptr = *ptr - 'a' + 'A';
+}
+ptr++;
+}
 
-    while (*ptr != '\0')
-    {
-        if (*ptr >= 'a' && *ptr <= 'z')
-        {
-            *ptr = *ptr - 'a' + 'A';
-        }
-        ptr++;
-    }
-
-    return str;
+return str;
 }
 
