2532bb91b2b2cb393ba71029ca51b4c206b8ac84
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-27 04:00:12 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* _strcat - Concatenates two strings.
+* @dest: Pointer to the destination string.
+* @src: Pointer to the source string.
+*
+* Return: Pointer to the resulting string 'dest'.
+*/
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
+
