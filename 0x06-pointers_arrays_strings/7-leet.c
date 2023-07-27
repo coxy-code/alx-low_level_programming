53f75ac65c47a68db8dae2cb13009d95db9bf886
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-27 04:34:09 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- * leet - Encodes a string into 1337.
- * @str: Pointer to the input string.
- *
- * Return: Pointer to the modified string.
- */
+* leet - Encodes a string into 1337.
+* @str: Pointer to the input string.
+*
+* Return: Pointer to the modified string.
+*/
 char *leet(char *str)
 {
-    char leet_map[] = "aeotlAEOTL";
-    char leet_replace[] = "4307143071";
-    char *ptr = str;
-    int i, j;
+char leet_map[] = "aeotlAEOTL";
+char leet_replace[] = "4307143071";
+char *ptr = str;
+int i, j;
 
-    while (*ptr != '\0')
-    {
-        i = 0;
-        j = 0;
-        while (leet_map[i] != '\0')
-        {
-            if (*ptr == leet_map[i])
-            {
-                *ptr = leet_replace[j];
-                break;
-            }
-            i++;
-            j++;
-        }
-        ptr++;
-    }
+while (*ptr != '\0')
+{
+i= 0;
+j = 0;
+while (leet_map[i] != '\0')
+{
+if (*ptr == leet_map[i])
+{
+*ptr = leet_replace[j];
+break;
+}
+i++;
+j++;
+}
+ptr++;
+}
 
-    return str;
+return str;
 }
 
