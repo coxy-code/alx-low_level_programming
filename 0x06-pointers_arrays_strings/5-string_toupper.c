765285a2fd7151209191a909e2ff307c6447fdc0
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-27 04:18:42 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * @str: Pointer to the input string.
+ *
+ * Return: Pointer to the modified string.
+ */
+char *string_toupper(char *str)
+{
+    char *ptr = str;
+
+    while (*ptr != '\0')
+    {
+        if (*ptr >= 'a' && *ptr <= 'z')
+        {
+            // Convert lowercase letter to uppercase.
+            *ptr = *ptr - 'a' + 'A';
+        }
+        ptr++;
+    }
+
+    return str;
+}
+
