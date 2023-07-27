fb25ecfafc6f1c7eae1d098fa1dc69b77a6958c1
coxy-code <kimutaicosmas.ck@gmail.com>
2023-07-27 06:31:14 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies a string up to n bytes.
- * @dest: Pointer to the destination buffer.
- * @src: Pointer to the source string.
- * @n: Number of bytes to copy.
- *
- * Return: Pointer to the destination buffer.
- */
+* _strncpy - Copies a string up to n bytes.
+* @dest: Pointer to the destination buffer.
+* @src: Pointer to the source string.
+* @n: Number of bytes to copy.
+*
+* Return: Pointer to the destination buffer.
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[i] = src[i];
-    }
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
 
-    while (i < n)
-    {
-        dest[i] = '\0';
-        i++;
-    }
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
 
-    return dest;
+return dest;
 }
 
