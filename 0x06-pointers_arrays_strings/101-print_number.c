94f2e89ac9c85c8dbe7e85ecd1edc0e8208255cb
coxy-code <cosmaskimutai.ck@gmail.com>
2023-07-28 14:44:34 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,15 @@
 #include "main.h"
 
-/* Function to print a single character */
-void _putchar(char c) {
-    /* Your implementation of _putchar here */
-}
-
-/* Recursive function to print a positive integer */
-void print_positive_number(unsigned int n) {
-    if (n / 10 != 0) {
-        print_positive_number(n / 10);
-    }
-    _putchar('0' + n % 10);
-}
-
-/* Function to print an integer */
+/* Recursive function to print an integer */
 void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    if (n == 0) {
-        _putchar('0');
-    } else {
-        print_positive_number(n);
-    }
+if (n < 0) {
+_putchar('-');
+n = -n;
 }
 
+if (n / 10 != 0) {
+print_number(n / 10);
+}
+
+_putchar('0' + n % 10);
+}
