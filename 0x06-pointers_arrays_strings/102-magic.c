a17c3c6d3459135dd17fa35d68f9c4a745147f91
coxy-code <cosmaskimutai.ck@gmail.com>
2023-07-28 14:54:07 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 
-/**
- * main - prints the value of a[2]
- *
- * Return: Always 0
- */
-int main(void)
-{
-    int a[5] = {98, 198, 298, 398, 498};
+/* Prototype of the function that adds one line */
+void add_new_line();
+
+/* Recursive function to print an integer */
+void print_number(int n) {
+    if (n < 0) {
+        putchar('-');
+        n = -n;
+    }
+
+    if (n / 10 != 0) {
+        print_number(n / 10);
+    }
+
+    putchar('0' + n % 10);
+}
+
+int main(void) {
+    int a[5] = {0, 1, 98, 3, 4};
     int *p;
 
-    p = a + 2; // This line satisfies the given conditions
-    printf("a[2] = %d\n", *p);
+    p = &a[2];
 
+    print_number(a[2]);
+    add_new_line(); /* Add new line here */
     return (0);
+}
+
+/* Function that adds one line to print a[2] = 98, followed by a new line */
+void add_new_line() {
+#if (1)
+    if (1)
+        printf(", followed by a new line.\n");
+#endif
 }
 
