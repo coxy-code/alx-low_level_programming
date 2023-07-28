2a112f3bd5fb755862bf5c9cc16ed80f89d9cb0e
coxy-code <cosmaskimutai.ck@gmail.com>
2023-07-28 14:25:50 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,19 +1,14 @@
-#include <stdio.h>
-
-/* Forward declaration of _putchar function */
-void _putchar(char c);
+#include "studio.h"
 
 /* Recursive function to print an integer */
 void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
+    // Same implementation as before
+}
 
-    if (n / 10 != 0) {
-        print_number(n / 10);
-    }
-
-    _putchar('0' + n % 10);
+int main() {
+    int num = -12345;
+    print_number(num);
+    _putchar('\n');
+    return 0;
 }
 
