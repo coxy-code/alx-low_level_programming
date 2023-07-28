7394bdd725697698f9de0145bf32abea0d3e130d
coxy-code <cosmaskimutai.ck@gmail.com>
2023-07-28 14:35:30 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,15 +2,14 @@
 
 /* Recursive function to print an integer */
 void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    if (n / 10 != 0) {
-        print_number(n / 10);
-    }
-
-    _putchar('0' + n % 10);
+if (n < 0) {
+_putchar('-');
+n = -n;
 }
 
+if (n / 10 != 0) {
+print_number(n / 10);
+}
+
+_putchar('0' + n % 10);
+}
