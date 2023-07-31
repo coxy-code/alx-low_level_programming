4b10993be97b2564e9148aec12eb233909da33ec
coxy-code <cosmaskimutai.ck@gmail.com>
2023-07-31 06:17:08 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,35 +1,18 @@
-#include <stddef.h>
+#include <stdio.h>
 #include "main.h"
 
-/**
- * _strstr - Locates a substring.
- * @haystack: The main string to search within.
- * @needle: The substring to find.
- *
- * Return: A pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
- */
-char *_strstr(char *haystack, char *needle)
+int main(void)
 {
-    char *haystack_ptr, *needle_ptr;
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
 
-    while (*haystack != '\0')
-    {
-        haystack_ptr = haystack;
-        needle_ptr = needle;
+    t = _strstr(s, f);
+    if (t != NULL)
+        printf("%s\n", t);
+    else
+        printf("Substring not found.\n");
 
-        while (*needle_ptr != '\0' && *haystack_ptr == *needle_ptr)
-        {
-            haystack_ptr++;
-            needle_ptr++;
-        }
-
-        if (*needle_ptr == '\0')
-            return haystack;
-
-        haystack++;
-    }
-
-    return NULL; // Substring not found
+    return 0;
 }
 
