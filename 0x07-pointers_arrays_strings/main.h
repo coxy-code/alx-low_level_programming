d6761758c75eecb0a1cddefb57d83c63a0c83203
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-01 01:35:46 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -21,7 +21,7 @@ char *_memset(char *s, char b, unsigned int n);
 /* Function prototypes */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-void simple_print_buffer(char *buffer, unsigned int size);
+void simple_print_buffer(char *buffer, unsigned int size)
 
 
 #endif /* MAIN_H */
