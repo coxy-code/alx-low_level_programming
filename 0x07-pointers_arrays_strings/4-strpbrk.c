bcff34458af4846ce611da2c78027bc87ab74098
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-01 00:16:36 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,22 +1,29 @@
 #include "main.h"
 
+/**
+* _strpbrk - Searches a string for any of a set of bytes.
+* @s: The input string to search.
+* @accept: The characters to search for.
+*
+* Return: A pointer to the first occurrence in @s of any of the bytes in
+*         the string @accept, or NULL if no such byte is found.
+*/
 char *_strpbrk(char *s, char *accept)
 {
-    while (*s != '\0')
-    {
-        char *a = accept;
+while (*s != '\0')
+{
+char *a = accept;
 
-        while (*a != '\0')
-        {
-            if (*s == *a)
-                return s;
+while (*a != '\0')
+{
+if (*s == *a)
+return s;
+a++;
+}
 
-            a++;
-        }
+s++;
+}
 
-        s++;
-    }
-
-    return NULL;
+return NULL;
 }
 
