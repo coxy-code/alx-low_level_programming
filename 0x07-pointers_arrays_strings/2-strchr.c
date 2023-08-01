247142f5454df0c6803adceec8f6046f61b28739
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-01 02:10:14 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,14 @@
  */
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; *s != '\0' i++)
-{
-if (*s == c)
-{
-return (s);
+    while (*s != '\0')
+    {
+        if (*s == c)
+        {
+            return s;
+        }
+        s++;
+    }
+    return NULL;
 }
-i++;
-}
-return (0);
+
