5c4eb43c9cebe72f9d3830cb509a944932450c79
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-01 01:07:29 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,12 @@
-#include <stddef.h>
 #include "main.h"
 
 /**
- * _strchr - Locates a character in a string.
- * @s: The string to search within.
- * @c: The character to find.
+ * _strchr - Locates the first occurrence of the character c in the string s.
+ * @s: Pointer to the string to be searched.
+ * @c: The character to be located.
  *
  * Return: A pointer to the first occurrence of the character c in the string s,
- * or NULL if the character is not found.
+ *         or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
@@ -18,9 +17,9 @@ char *_strchr(char *s, char c)
         s++;
     }
 
-    if (*s == c)
+    if (*s == c) /* Check the null terminator separately */
         return s;
 
-    return NULL; // Character not found
+    return NULL; /* Character not found */
 }
 
