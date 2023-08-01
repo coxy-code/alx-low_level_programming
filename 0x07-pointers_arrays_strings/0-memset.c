0a384969330f99c190f49f4c8c6e63013fb82666
coxy-code <cosmaskimutai.ck@gmail.com>
2023-07-31 23:13:05 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _memset - Fills memory with a constant byte.
+ * custom_memset - Fills memory with a constant byte.
  * @s: Pointer to the memory area to be filled.
  * @b: The constant byte to fill the memory area with.
  * @n: The number of bytes to fill.
  *
  * Return: A pointer to the memory area s.
  */
-char *_memset(char *s, char b, unsigned int n)
+char *custom_memset(char *s, char b, unsigned int n)
 {
     unsigned int i;
 
