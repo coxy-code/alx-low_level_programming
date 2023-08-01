2835bb73e7c725982ded0529b1e57631613ce3e7
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-01 00:50:48 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+* set_string - Sets the value of a pointer to a string.
+* @s: A pointer to a pointer to char (string).
+* @to: The string to set the pointer to.
+*
+* Return: Nothing (void).
+*/
 void set_string(char **s, char *to)
 {
-    *s = to;
+*s = to;
 }
 
