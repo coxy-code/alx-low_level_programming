ad3589e86f3e1cf7d15973adfcd57f32702178c6
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-01 00:21:14 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+* print_chessboard - Prints a chessboard of 8x8 grid.
+* @a: The 2D array representing the chessboard.
+*
+* Return: Nothing (void).
+*/
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            putchar(a[i][j]);
-        }
-        putchar('\n');
-    }
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+putchar(a[i][j]);
+}
+putchar('\n');
+}
 }
 
