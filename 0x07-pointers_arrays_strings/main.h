369f65fdbd8e3f6a71daf2c199175140b3327cec
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-01 01:22:20 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+#include <stdio.h>
 #include <stddef.h> /* For size_t */
 
 char *_memcpy(char *dest, char *src, size_t n);
@@ -18,9 +20,8 @@ char *_memset(char *s, char b, unsigned int n);
 /* Function prototypes */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-void print_chessboard(char (*a)[8]);
 void simple_print_buffer(char *buffer, unsigned int size);
-void set_string(char **s, char *to);
+
 
 #endif /* MAIN_H */
 
