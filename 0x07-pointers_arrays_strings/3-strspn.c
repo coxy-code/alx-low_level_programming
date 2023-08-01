8f1e7e067ee449647a69bf039c9e974b19f1e0e8
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-01 00:12:43 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,29 +1,37 @@
 #include "main.h"
 
+/**
+* _strspn - Gets the length of a prefix substring that consists of only
+* characters present in accept.
+* @s: The input string.
+* @accept: The characters to be accepted.
+*
+* Return: The number of bytes in the initial segment of s which consist
+*         only of bytes from accept.
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
+unsigned int count = 0;
 
-    while (*s != '\0')
-    {
-        char *a = accept;
+while (*s != '\0')
+{
+char *a = accept;
 
-        while (*a != '\0')
-        {
-            if (*s == *a)
-            {
-                count++;
-                break;
-            }
-            a++;
-        }
-
-        if (*a == '\0')
-            break;
-
-        s++;
-    }
-
-    return count;
+while (*a != '\0')
+{
+if (*s == *a)
+{
+count++;
+break;
+}
+a++;
 }
 
+if (*a == '\0')
+break;
+
+s++;
+}
+
+return (count);
+}
