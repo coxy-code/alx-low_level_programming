9d1d97c12a01c8ddb5863f00b6a1dbd0c8c6947b
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-02 13:03:01 -0700

    recussion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+* sqrt_check - Checks for the square root of n using recursion.
+* @n: The number for which the square root is to be found.
+* @i: The current number to check for square root.
+* Return: The square root of n. If n does not have a natural square root, return -1.
+*/
+int sqrt_check(int n, int i)
+{
+if (i * i == n)
+{
+/* If i squared is equal to n, i is the square root of n. */
+return (i);
+}
+else if (i * i > n)
+{
+/* If i squared exceeds n, n does not have a natural square root. */
+return (-1);
+}
+else
+{
+/* Recursive call with i incremented to check the next number. */
+return (sqrt_check(n, i + 1));
+}
+}
+
+/**
+* _sqrt_recursion - Returns the natural square root of a number using recursion.
+* @n: The number for which the square root is to be found.
+* Return: The square root of n. If n does not have a natural square root, return -1.
+*/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+/* If n is negative, return -1 to indicate an error. */
+return (-1);
+}
+else
+{
+/* Start checking for square root from 1. */
+return (sqrt_check(n, 1));
+}
+}
+
