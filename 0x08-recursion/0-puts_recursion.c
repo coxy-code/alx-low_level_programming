1feb1b4da904366cd414528b28a9db3bfcce6a0b
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-02 12:01:42 -0700

    recussion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,4 @@
 #include "main.h"
-#include <unistd.h> /* Include the necessary header for the write function */
-
-/**
- * _putchar - Custom function to print a single character.
- * @c: The character to be printed.
- * Return: On success, 1. On error, -1.
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
 
 /**
  * _puts_recursion - Prints a string, followed by a new line, using recursion.
