138037d11894940a29d249420b4ca87c4657ad56
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-02 12:17:15 -0700

    recussion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse using recursion.
- * @s: The input string to be printed in reverse.
- */
+* _print_rev_recursion - Prints a string in reverse using recursion.
+* @s: The input string to be printed in reverse.
+*/
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        /* If we reach the end of the string, return. */
-        return;
-    }
+if (*s == '\0')
+{
+/* If we reach the end of the string, return. */
+return;
+}
 
-    /* Recursive call with the next character in the string. */
-    _print_rev_recursion(s + 1);
+/* Recursive call with the next character in the string. */
+_print_rev_recursion(s + 1);
 
-    /* Print the current character after the recursive call (in reverse). */
-    _putchar(*s);
+/* Print the current character after the recursive call (in reverse). */
+_putchar(*s);
 }
 
