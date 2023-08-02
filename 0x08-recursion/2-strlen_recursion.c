ffe2cca731108c800dbf287bf60e6df4622314df
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-02 12:23:40 -0700

    recussion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of a string using recursion.
- * @s: The input string for which length is to be calculated.
- * Return: The length of the string.
- */
+* _strlen_recursion - Returns the length of a string using recursion.
+* @s: The input string for which length is to be calculated.
+* Return: The length of the string.
+*/
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        /* If we reach the end of the string, return 0. */
-        return 0;
-    }
-    else
-    {
-        /* Recursive call with the next character in the string,
-         * and add 1 to the result (for the current character). */
-        return 1 + _strlen_recursion(s + 1);
-    }
+if (*s == '\0')
+{
+/* If we reach the end of the string, return 0. */
+return 0;
+}
+else
+{
+/* Recursive call with the next character in the string,
+* and add 1 to the result (for the current character). */
+return 1 + _strlen_recursion(s + 1);
+}
 }
 
