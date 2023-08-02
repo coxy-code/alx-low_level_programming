d279a2fb951ef453c60467976f9af9cd949f64ed
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-02 12:44:48 -0700

    recussion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * factorial - Returns the factorial of a given number.
- * @n: The number for which the factorial is to be calculated.
- * Return: The factorial of n. If n is lower than 0, return -1 (error).
- */
+* factorial - Returns the factorial of a given number.
+* @n: The number for which the factorial is to be calculated.
+* Return: The factorial of n. If n is lower than 0, return -1 (error).
+*/
 int factorial(int n)
 {
-    if (n < 0)
-    {
-        /* If n is lower than 0, return -1 to indicate an error. */
-        return -1;
-    }
-    else if (n == 0)
-    {
-        /* Factorial of 0 is 1. */
-        return 1;
-    }
-    else
-    {
-        /* Recursive call with n-1, multiplied by n (current number). */
-        return n * factorial(n - 1);
-    }
+if (n < 0)
+{
+/* If n is lower than 0, return -1 to indicate an error. */
+return (-1);
+}
+else if (n == 0)
+{
+/* Factorial of 0 is 1. */
+return (1);
+}
+else
+{
+/* Recursive call with n-1, multiplied by n (current number). */
+return (n * factorial(n - 1));
+}
 }
 
