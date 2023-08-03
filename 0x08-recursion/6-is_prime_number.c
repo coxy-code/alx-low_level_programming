e075d74d72d749c66a3b0b8b1513c51f112ee7d6
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-03 00:03:38 -0700

    recussion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,19 +10,20 @@ int is_prime_number(int n)
 {
 /* Base cases */
 if (n <= 1) {
-return 0; /* Neither prime nor composite*/
+return 0; /* Neither prime nor composite */
 }
 if (n <= 3) {
-return 1; /* 2 and 3 are prime*/
+return 1; /* 2 and 3 are prime */
 }
 
 /* Check divisibility by numbers from 2 to sqrt(n) */
 int i;
 for (i = 2; i * i <= n; i++) {
 if (n % i == 0) {
-return 0; /*The number is divisible, so it's not prime*/
+return 0; /* The number is divisible, so it's not prime */
 }
 }
-return 1; /*The number is prime*/
+
+return 1; /* The number is prime*/
 }
 
