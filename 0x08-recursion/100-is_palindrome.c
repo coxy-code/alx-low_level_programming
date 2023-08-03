b98d4948644b06837577faf98b18247b7827fc27
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-03 14:04:51 -0700

    recussion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,37 @@
 #include "main.h"
 
 /**
+ * _strlen - Calculates the length of a string.
+ * @s: The string to calculate the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+    if (*s == '\0')
+        return 0;
+    else
+        return 1 + _strlen(s + 1);
+}
+
+/**
+ * _compare_chars - Helper function to compare characters for palindrome check.
+ * @s: The string to check.
+ * @start: The starting index.
+ * @end: The ending index.
+ *
+ * Return: 1 if the characters are the same, 0 otherwise.
+ */
+int _compare_chars(char *s, int start, int end)
+{
+    if (start >= end)
+        return 1;
+    if (s[start] != s[end])
+        return 0;
+    return _compare_chars(s, start + 1, end - 1);
+}
+
+/**
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The input string.
  *
@@ -8,30 +39,7 @@
  */
 int is_palindrome(char *s)
 {
-    int length = 0;
-    int i;
-
-    /* Calculate the length of the string */
-    while (s[length] != '\0')
-    {
-        length++;
-    }
-
-    /* Base case: An empty string is a palindrome */
-    if (length <= 1)
-    {
-        return 1;
-    }
-
-    /* Compare characters from both ends of the string */
-    for (i = 0; i < length / 2; i++)
-    {
-        if (s[i] != s[length - 1 - i])
-        {
-            return 0; /* Not a palindrome */
-        }
-    }
-
-    return 1; /* The string is a palindrome */
+    int len = _strlen(s);
+    return _compare_chars(s, 0, len - 1);
 }
 
