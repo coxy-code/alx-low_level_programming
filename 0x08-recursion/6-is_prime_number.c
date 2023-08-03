5c55d35cf828ed44132ec5e14a7b054aea5ba7b5
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-03 13:21:03 -0700

    recussion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
-* is_prime_number - Checks if a given number is a prime number.
-* @n: The number to check.
-*
-* Return: 1 if the number is prime, 0 otherwise.
-*/
+ * is_prime_number - Checks if a given number is a prime number.
+ * @n: The number to check.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
 int is_prime_number(int n)
 {
-/* Base cases */
-if (n <= 1) {
-return 0; /* Neither prime nor composite */
-}
-if (n <= 3) {
-return 1; /* 2 and 3 are prime */
+    /* Base cases */
+    if (n <= 1)
+        return 0; /* Neither prime nor composite */
+    if (n <= 3)
+        return 1; /* 2 and 3 are prime */
+
+    /* Check divisibility by numbers from 2 to sqrt(n) */
+    int i;
+    for (i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return 0; /* The number is divisible, so it's not prime */
+    }
+
+    return 1; /* The number is prime*/
 }
 
-/* Check divisibility by numbers from 2 to sqrt(n) */
-int i;
-for (i = 2; i * i <= n; i++) {
-if (n % i == 0) {
-return 0; /* The number is divisible, so it's not prime */
-}
-}
-
-return 1; /* The number is prime*/
-}
+/* Additional functions or code can go here if needed. */
 
