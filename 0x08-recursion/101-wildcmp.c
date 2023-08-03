b6d1bb4f2ffe411439f8a19ab4e10cd0d61c0586
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-03 14:34:46 -0700

    recussion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,23 +12,23 @@ int wildcmp(char *s1, char *s2)
 if (*s2 == '*')
 {
 if (*(s2 + 1) == '*')
-return wildcmp(s1, s2 + 1);
+return (wildcmp(s1, s2 + 1));
 else if (*(s2 + 1) == '\0')
-return 1;
+return (1);
 else if (*s1 == '\0')
-return wildcmp(s1, s2 + 1);
+return (wildcmp(s1, s2 + 1));
 else if (*s1 == *(s2 + 1))
-return wildcmp(s1 + 1, s2 + 2) || wildcmp(s1 + 1, s2);
+return (wildcmp(s1 + 1, s2 + 2) || wildcmp(s1 + 1, s2));
 else
-return wildcmp(s1 + 1, s2);
+return (wildcmp(s1 + 1, s2));
 }
 if (*s1 == *s2)
 {
 if (*s1 == '\0')
-return 1;
-return wildcmp(s1 + 1, s2 + 1);
+return (1);
+return (wildcmp(s1 + 1, s2 + 1));
 }
 
-return 0;
+return (0);
 }
 
