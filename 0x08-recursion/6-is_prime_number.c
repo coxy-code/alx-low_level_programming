89d9dd9fb144d6c746bb938967f29cc78ca32fc4
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-03 14:39:00 -0700

    recussion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,34 @@
 #include "main.h"
 
 /**
- * is_prime_number - Checks if a given number is a prime number.
- * @n: The number to check.
+ * is_prime_helper - Helper function to check if a number is prime.
+ * @n: The number to check for primality.
+ * @divisor: The current divisor to test.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
+int is_prime_helper(int n, int divisor)
+{
+    if (n < 2) // 0 and 1 are not prime numbers
+        return 0;
+
+    if (divisor * divisor > n) // All divisors checked, it's a prime number
+        return 1;
+
+    if (n % divisor == 0) // n is divisible by divisor, not a prime number
+        return 0;
+
+    return is_prime_helper(n, divisor + 1); // Check next divisor
+}
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The input number to check.
  *
  * Return: 1 if the number is prime, 0 otherwise.
  */
 int is_prime_number(int n)
 {
-    /* Base cases */
-    if (n <= 1)
-        return 0; /* Neither prime nor composite */
-    if (n <= 3)
-        return 1; /* 2 and 3 are prime */
-
-    /* Check divisibility by numbers from 2 to sqrt(n) */
-    int i;
-    for (i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0)
-            return 0; /* The number is divisible, so it's not prime */
-    }
-
-    return 1; /* The number is prime*/
+    return is_prime_helper(n, 2); // Start checking from 2 (the first prime number)
 }
 
