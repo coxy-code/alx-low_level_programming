badbc3e55af516f61868be45a92b004d57398562
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-04 13:36:34 -0700

    recussion

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    (void)argc; // Suppress unused parameter warning
+    (void)argc; /* Suppress unused parameter warning */
     printf("%s\n", argv[0]);
     return 0;
 }
