d3cad43a2919c1ac00691d35da339b84addc9b2e
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-04 14:16:56 -0700

    recussion

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
- *
- * Return: Always 0
- */
+* main - Entry point of the program
+* @argc: The number of command-line arguments
+* @argv: An array of strings containing the arguments
+*
+* Return: Always 0
+*/
 int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
-    return 0;
+(void)argc; /* Suppress unused parameter warning */
+printf("%s\n", argv[0]);
+return 0;
 }
 
