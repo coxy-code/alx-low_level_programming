3448d0e7e50ac1e6bb1b4029c3543bbf179d7cba
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-07 05:02:18 -0700

    recussion

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,11 +6,11 @@ char *create_array(unsigned int size, char c)
         return NULL;
 
     char *array = (char *)malloc(sizeof(char) * size);
-
     if (array == NULL)
         return NULL;
 
-    for (unsigned int i = 0; i < size; i++)
+    unsigned int i; // Move the declaration here
+    for (i = 0; i < size; i++)
         array[i] = c;
 
     return array;
