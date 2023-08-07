526ad33e3397119420d6ae349e5dbca73908dd2b
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-07 09:20:25 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,7 +9,7 @@ int count_words(char *str) {
     int count = 0;
     int i = 0;
 
-    while (str[i]) {
+    while (str[i] != '\0') {
         while (is_delimiter(str[i]))
             i++;
 
@@ -18,7 +18,7 @@ int count_words(char *str) {
 
         count++;
 
-        while (str[i] && !is_delimiter(str[i]))
+        while (str[i] != '\0' && !is_delimiter(str[i]))
             i++;
     }
 
@@ -26,33 +26,31 @@ int count_words(char *str) {
 }
 
 char **strtow(char *str) {
+    int word_count, i, j, k, word_length;
+    char **words;
+
     if (str == NULL || *str == '\0')
         return NULL;
 
-    int word_count = count_words(str);
+    word_count = count_words(str);
     if (word_count == 0)
         return NULL;
 
-    char **words = malloc((word_count + 1) * sizeof(char *));
+    words = (char **)malloc((word_count + 1) * sizeof(char *));
     if (words == NULL)
         return NULL;
 
-    int i = 0;
-    int word_index = 0;
-
-    while (str[i]) {
+    i = 0;
+    for (int word_index = 0; word_index < word_count; word_index++) {
         while (is_delimiter(str[i]))
             i++;
 
-        if (str[i] == '\0')
-            break;
-
-        int j = i;
-        while (str[j] && !is_delimiter(str[j]))
+        j = i;
+        while (str[j] != '\0' && !is_delimiter(str[j]))
             j++;
 
-        int word_length = j - i;
-        words[word_index] = malloc((word_length + 1) * sizeof(char));
+        word_length = j - i;
+        words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
         if (words[word_index] == NULL) {
             while (word_index > 0) {
                 free(words[word_index - 1]);
@@ -62,14 +60,13 @@ char **strtow(char *str) {
             return NULL;
         }
 
-        for (int k = 0; k < word_length; k++)
+        for (k = 0; k < word_length; k++)
             words[word_index][k] = str[i++];
 
         words[word_index][word_length] = '\0';
-        word_index++;
     }
 
-    words[word_index] = NULL;
+    words[word_count] = NULL;
 
     return words;
 }
