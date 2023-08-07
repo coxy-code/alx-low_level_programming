56b75b1c493a6e7824dae071c129aac4947806c6
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-07 05:26:35 -0700

    malloc

--- 0x0B-malloc_free/main_0.c
+++ 0x0B-malloc_free/main_0.c
@@ -2,28 +2,33 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+* main - Entry point.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    unsigned int size = 10; /* Adjust the size as needed*/
-    char c = 'H'; /* Adjust the character as needed*/
+unsigned int size = 10; /* Adjust the size as needed*/
+char c = 'H'; /* Adjust the character as needed*/
 
-    char *array = create_array(size, c);
+char *array = create_array(size, c);
 
-    if (array == NULL)
-    {
-        printf("Memory allocation failed\n");
-        return 1;
-    }
+if (array == NULL)
+{
+printf("Memory allocation failed\n");
+return (1);
+}
 
-    /* Print the contents of the array*/
-    for (unsigned int i = 0; i < size; i++)
-    {
-        printf("%c", array[i]);
-    }
-    printf("\n");
+/* Print the contents of the array*/
+for (unsigned int i = 0; i < size; i++)
+{
+printf("%c", array[i]);
+}
+printf("\n");
 
-    free(array);
+free(array);
 
-    return 0;
+return (0);
 }
 
