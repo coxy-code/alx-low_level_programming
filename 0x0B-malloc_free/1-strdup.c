0e975a12210953fe471fe91a59e17298b5df7934
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-07 05:39:30 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,32 @@
 #include <stdlib.h>
 
 /**
- * _strdup - Creates a duplicate of a string.
- * @str: The string to be duplicated.
- *
- * Return: Pointer to the duplicated string, or NULL if insufficient memory.
- */
+* _strdup - Creates a duplicate of a string.
+* @str: The string to be duplicated.
+*
+* Return: Pointer to the duplicated string, or NULL if insufficient memory.
+*/
 char *_strdup(char *str)
 {
-    char *duplicate;
-    unsigned int i, length = 0;
+char *duplicate;
+unsigned int i, length = 0;
 
-    if (str == NULL)
-        return (NULL);
+if (str == NULL)
+return (NULL);
 
-    /* Calculate the length of the input string*/
-    while (str[length] != '\0')
-        length++;
+/* Calculate the length of the input string*/
+while (str[length] != '\0')
+length++;
 
-    /* Allocate memory for the duplicate string*/
-    duplicate = (char *)malloc(sizeof(char) * (length + 1));
-    if (duplicate == NULL)
-        return (NULL);
+/* Allocate memory for the duplicate string*/
+duplicate = (char *)malloc(sizeof(char) * (length + 1));
+if (duplicate == NULL)
+return (NULL);
 
-    /* Copy the characters from the input string to the duplicate*/
-    for (i = 0; i <= length; i++)
-        duplicate[i] = str[i];
+/* Copy the characters from the input string to the duplicate*/
+for (i = 0; i <= length; i++)
+duplicate[i] = str[i];
 
-    return (duplicate);
+return (duplicate);
 }
 
