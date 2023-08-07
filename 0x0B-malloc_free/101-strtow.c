a80984ce30506084a28ab7ffed86a6388ac49afd
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-07 09:37:22 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,52 +1,130 @@
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-char **strtow(char *str) {
-    int word_count, i, j, k, word_length, word_index;
-    char **words;
+/**
+ * count_words - Counts the number of words in a string.
+ * @str: The input string.
+ *
+ * Return: The number of words.
+ */
+int count_words(char *str)
+{
+    int count = 0;
+    bool in_word = false;
 
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != ' ')
+        {
+            if (!in_word)
+            {
+                in_word = true;
+                count++;
+            }
+        }
+        else
+        {
+            in_word = false;
+        }
+    }
+
+    return count;
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The input string.
+ *
+ * Return: Pointer to an array of strings (words), or NULL on failure.
+ */
+char **strtow(char *str)
+{
     if (str == NULL || *str == '\0')
         return NULL;
 
-    word_count = count_words(str); // Define count_words or include its declaration
-
-    if (word_count == 0)
-        return NULL;
-
-    words = (char **)malloc((word_count + 1) * sizeof(char *));
-
+    int num_words = count_words(str);
+    char **words = (char **)malloc((num_words + 1) * sizeof(char *));
     if (words == NULL)
         return NULL;
 
-    i = 0;
-    for (word_index = 0; word_index < word_count; word_index++) {
-        while (is_delimiter(str[i]))
-            i++;
+    int word_index = 0;
+    bool in_word = false;
+    int word_start = 0;
 
-        j = i;
-        while (str[j] != '\0' && !is_delimiter(str[j]))
-            j++;
-
-        word_length = j - i;
-        words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-
-        if (words[word_index] == NULL) {
-            while (word_index > 0) {
-                free(words[word_index - 1]);
-                word_index--;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != ' ')
+        {
+            if (!in_word)
+            {
+                in_word = true;
+                word_start = i;
             }
+        }
+        else
+        {
+            if (in_word)
+            {
+                in_word = false;
+                int word_length = i - word_start;
+                words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+                if (words[word_index] == NULL)
+                {
+                    /* Free previously allocated memory */
+                    for (int j = 0; j < word_index; j++)
+                        free(words[j]);
+                    free(words);
+                    return NULL;
+                }
+
+                strncpy(words[word_index], str + word_start, word_length);
+                words[word_index][word_length] = '\0';
+                word_index++;
+            }
+        }
+    }
+
+    if (in_word)
+    {
+        int word_length = strlen(str) - word_start;
+        words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+        if (words[word_index] == NULL)
+        {
+            /* Free previously allocated memory */
+            for (int j = 0; j <= word_index; j++)
+                free(words[j]);
             free(words);
             return NULL;
         }
 
-        for (k = 0; k < word_length; k++)
-            words[word_index][k] = str[i++];
-
+        strncpy(words[word_index], str + word_start, word_length);
         words[word_index][word_length] = '\0';
+        word_index++;
     }
 
-    words[word_count] = NULL;
+    words[word_index] = NULL;
 
     return words;
+}
+
+int main(void)
+{
+    char **tab;
+
+    tab = strtow("      ALX School         #cisfun      ");
+    if (tab == NULL)
+    {
+        printf("Failed\n");
+        return (1);
+    }
+    for (int i = 0; tab[i] != NULL; i++)
+    {
+        printf("%s$\n", tab[i]);
+        free(tab[i]);
+    }
+    free(tab);
+    return (0);
 }
 
