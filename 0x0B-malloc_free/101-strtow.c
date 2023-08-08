afc986dd0aa69f17719c595f4ad88242757c8810
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-08 02:03:50 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,119 +1,105 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * count_words - Count the number of words in a string
+ * @str: The input string
+ *
+ * Return: The number of words in the string
+ */
 int count_words(char *str)
 {
     int count = 0;
-    bool in_word = false;
-    int i;
+    int is_word = 0;
 
-    for (i = 0; str[i] != '\0'; i++)
+    while (*str != '\0')
     {
-        if (str[i] != ' ')
+        if (*str == ' ')
         {
-            if (!in_word)
-            {
-                in_word = true;
-                count++;
-            }
+            is_word = 0;
         }
-        else
+        else if (is_word == 0)
         {
-            in_word = false;
+            is_word = 1;
+            count++;
         }
+        str++;
     }
 
     return count;
 }
 
+/**
+ * strtow - Split a string into words
+ * @str: The input string
+ *
+ * Return: Pointer to an array of strings (words)
+ */
 char **strtow(char *str)
 {
     if (str == NULL || *str == '\0')
         return NULL;
 
     int num_words = count_words(str);
-    char **words = (char **)malloc((num_words + 1) * sizeof(char *));
+
+    char **words = malloc((num_words + 1) * sizeof(char *));
     if (words == NULL)
         return NULL;
 
+    int word_len = 0;
     int word_index = 0;
-    bool in_word = false;
-    int word_start = 0;
-    int i;
+    int i, j;
 
     for (i = 0; str[i] != '\0'; i++)
     {
         if (str[i] != ' ')
         {
-            if (!in_word)
-            {
-                in_word = true;
-                word_start = i;
-            }
+            word_len++;
         }
-        else
+        else if (word_len > 0)
         {
-            if (in_word)
+            words[word_index] = malloc((word_len + 1) * sizeof(char));
+            if (words[word_index] == NULL)
             {
-                int word_length = i - word_start;
-                words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-                if (words[word_index] == NULL)
-                {
-                    for (int j = 0; j < word_index; j++)
-                        free(words[j]);
-                    free(words);
-                    return NULL;
-                }
-
-                strncpy(words[word_index], str + word_start, word_length);
-                words[word_index][word_length] = '\0';
-                word_index++;
-                in_word = false;
+                /* Free previously allocated memory */
+                for (j = 0; j < word_index; j++)
+                    free(words[j]);
+                free(words);
+                return NULL;
             }
+
+            /* Copy the word to the allocated memory */
+            for (j = 0; j < word_len; j++)
+                words[word_index][j] = str[i - word_len + j];
+            words[word_index][word_len] = '\0';
+
+            word_index++;
+            word_len = 0;
         }
     }
 
-    if (in_word)
+    /* Handle the last word if exists */
+    if (word_len > 0)
     {
-        int word_length = i - word_start;
-        words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+        words[word_index] = malloc((word_len + 1) * sizeof(char));
         if (words[word_index] == NULL)
         {
-            for (int j = 0; j <= word_index; j++)
+            /* Free previously allocated memory */
+            for (j = 0; j <= word_index; j++)
                 free(words[j]);
             free(words);
             return NULL;
         }
 
-        strncpy(words[word_index], str + word_start, word_length);
-        words[word_index][word_length] = '\0';
-        word_index++;
+        /* Copy the last word to the allocated memory */
+        for (j = 0; j < word_len; j++)
+            words[word_index][j] = str[i - word_len + j];
+        words[word_index][word_len] = '\0';
     }
 
-    words[word_index] = NULL;
+    words[num_words] = NULL;
 
     return words;
-}
-
-int main(void)
-{
-    char **tab;
-
-    tab = strtow("      ALX School         #cisfun      ");
-    if (tab == NULL)
-    {
-        printf("Failed\n");
-        return (1);
-    }
-    int i;
-    for (i = 0; tab[i] != NULL; i++)
-    {
-        printf("%s$\n", tab[i]);
-        free(tab[i]);
-    }
-    free(tab);
-    return (0);
 }
 
