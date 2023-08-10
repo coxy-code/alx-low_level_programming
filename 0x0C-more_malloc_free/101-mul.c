ac06bbaa5e0236bb4139ca36c8cced56ec869fbd
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-10 09:07:28 -0700

    malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,100 +1,100 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int _putchar(char c);
+int find_len(char *str);
+char *create_array(int size);
+char *iterate_zeroes(char *str);
+void get_product(char *product, char *multiplier, int digit, int zeroes);
+void add_numbers(char *final_product, char *next_product, int next_len);
 
-/**
-* print_error_and_exit - Prints an error message and exits with status 98.
-*/
-void print_error_and_exit(void)
-{
-_putchar('E');
-_putchar('r');
-_putchar('r');
-_putchar('o');
-_putchar('r');
-_putchar('\n');
-exit(98);
-}
-
-/**
-* print_result - Prints the result of the multiplication.
-* @result: The result to be printed.
-*/
-void print_result(char *result)
-{
-int i = 0;
-while (result[i] == '0')
-i++;
-
-if (result[i] == '\0')
-_putchar('0');
-else
-{
-while (result[i])
-{
-_putchar(result[i]);
-i++;
-}
-}
-_putchar('\n');
-}
-
-/**
-* main - Entry point. Multiplies two positive numbers.
-* @argc: The number of command-line arguments.
-* @argv: An array of strings containing the arguments.
-* Return: 0 on success, 1 on failure.
-*/
 int main(int argc, char *argv[])
 {
-int len1, len2;
-char *result;
-int i, j, carry;
-if (argc != 3)
-print_error_and_exit();
-len1 = 0;
-while (argv[1][len1])
+if (argc != 3) {
+fprintf(stderr, "Usage: %s num1 num2\n", argv[0]);
+return 1;
+}
+char *num1 = argv[1];
+char *num2 = argv[2];
+    
+int len1 = find_len(num1);
+int len2 = find_len(num2);
+
+if (len1 == 0 || len2 == 0) {
+fprintf(stderr, "Error: Invalid input numbers.\n");
+return 1;
+}
+
+int size = len1 + len2;
+char *product = create_array(size);
+char *next_product = create_array(size);
+
+for (int i = len2 - 1; i >= 0; i--) {
+int digit = num2[i] - '0';
+next_product = iterate_zeroes(next_product);
+get_product(next_product, num1, digit, len2 - i - 1);
+add_numbers(product, next_product, size);
+}
+
+print_result(product);
+free(product);
+free(next_product);
+
+return 0;
+}
+
+int find_len(char *str)
 {
-if (argv[1][len1] < '0' || argv[1][len1] > '9')
-print_error_and_exit();
-len1++;
+int len = 0;
+while (str[len] != '\0')
+len++;
+return len;
 }
 
-len2 = 0;
-while (argv[2][len2])
+char *create_array(int size)
 {
-if (argv[2][len2] < '0' || argv[2][len2] > '9')
-print_error_and_exit();
-len2++;
+char *arr = malloc(sizeof(char) * (size + 1));
+if (arr == NULL) {
+fprintf(stderr, "Memory allocation failed.\n");
+exit(1);
+}
+for (int i = 0; i <= size; i++)
+arr[i] = '0';
+arr[size] = '\0';
+return arr;
 }
 
-if (len1 == 0 || len2 == 0)
-print_error_and_exit();
-
-result = malloc(sizeof(char) * (len1 + len2 + 1));
-if (result == NULL)
-return (1);
-
-for (i = 0; i < len1 + len2; i++)
-result[i] = '0';
-result[i] = '\0';
-for (i = len1 - 1; i >= 0; i--)
+char *iterate_zeroes(char *str)
 {
-carry = 0;
-for (j = len2 - 1; j >= 0; j--)
+int len = find_len(str);
+for (int i = len - 1; i >= 0; i--)
+str[i + 1] = str[i];
+str[0] = '0';
+return str;
+}
+
+void get_product(char *product, char *multiplier, int digit, int zeroes)
 {
-carry = (result[i + j + 1] - '0' + ((argv[1][i] - '0')
-* (argv[2][j] - '0')) + carry) / 10;
-result[i + j + 1] = ((result[i + j + 1] - '0') +((argv[1][i] - '0') 
-* (argv[2][j] - '0')) + carry) % 10 + '0';
+int carry = 0;
+int len = find_len(multiplier);
+for (int i = len - 1; i >= 0; i--) {
+int num = (multiplier[i] - '0') * digit + carry;
+carry = num / 10;
+num %= 10;
+product[i + zeroes] = num + '0';
 }
-result[i + j + 1] = carry + '0';
-}
-
-print_result(result);
-free(result);
-
-return (0);
+if (carry > 0)
+product[zeroes - 1] = carry + '0';
 }
 
+void add_numbers(char *final_product, char *next_product, int next_len)
+{
+int carry = 0;
+int len = find_len(final_product);
+for (int i = len - 1; i >= 0; i--) {
+int sum = (final_product[i] - '0') + (next_product[i] - '0') + carry;
+carry = sum / 10;
+sum %= 10;
+final_product[i] = sum + '0';
+}
+}
