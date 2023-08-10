5b1d747a9189f2b2b039139ab485cd089d7b4682
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-10 05:45:50 -0700

    malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,34 +1,34 @@
 #include <stdlib.h>
 
 /**
- * _calloc - Allocates memory for an array and initializes it with zeros.
- * @nmemb: The number of elements in the array.
- * @size: The size of each element.
- *
- * Return: A pointer to the allocated memory, or NULL on failure.
- */
+* _calloc - Allocates memory for an array and initializes it with zeros.
+* @nmemb: The number of elements in the array.
+* @size: The size of each element.
+*
+* Return: A pointer to the allocated memory, or NULL on failure.
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
-    unsigned int total_size;
-    unsigned int i;
+void *ptr;
+unsigned int total_size;
+unsigned int i;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+if (nmemb == 0 || size == 0)
+return (NULL);
 
-    total_size = nmemb * size;
+total_size = nmemb * size;
 
-    ptr = malloc(total_size);
+ptr = malloc(total_size);
 
-    if (ptr == NULL)
-        return (NULL);
+if (ptr == NULL)
+return (NULL);
 
-    /* Set the allocated memory to zero */
-    for (i = 0; i < total_size; i++)
-    {
-        *((char *)ptr + i) = 0;
-    }
+/* Set the allocated memory to zero */
+for (i = 0; i < total_size; i++)
+{
+*((char *)ptr + i) = 0;
+}
 
-    return (ptr);
+return (ptr);
 }
 
