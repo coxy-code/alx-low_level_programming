71e70fe016889e8195f5258f0ce6a65addfe779f
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-10 06:00:57 -0700

    malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,35 +1,45 @@
-#include "main.h"
+#include <stdlib.h>
 
+/**
+* _realloc - Reallocates a memory block.
+* @ptr: A pointer to the memory block to be reallocated.
+* @old_size: The size of the old memory block.
+* @new_size: The size of the new memory block.
+*
+* Return: A pointer to the reallocated memory block, or NULL on failure or invalid input.
+*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    void *new_ptr;
+void *new_ptr;
+unsigned int i;
 
-    if (new_size == old_size)
-        return (ptr);
+if (new_size == old_size)
+return (ptr);
 
-    if (ptr == NULL)
-        return (malloc(new_size));
+if (ptr == NULL)
+return (malloc(new_size));
 
-    if (new_size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
+if (new_size == 0)
+{
+free(ptr);
+return (NULL);
+}
 
-    new_ptr = malloc(new_size);
+new_ptr = malloc(new_size);
 
-    if (new_ptr == NULL)
-        return (NULL);
+if (new_ptr == NULL)
+return (NULL);
 
-    char *byte_ptr = (char *)ptr;
-    char *new_byte_ptr = (char *)new_ptr;
-    unsigned int min_size = (old_size < new_size) ? old_size : new_size;
+if (new_size > old_size)
+new_size = old_size;
 
-    for (unsigned int i = 0; i < min_size; i++)
-        new_byte_ptr[i] = byte_ptr[i];
+for (i = 0; i < new_size; i++)
+{
+*((char *)new_ptr + i) = *((char *)ptr + i);
+}
 
-    free(ptr);
+free(ptr);
 
-    return (new_ptr);
+return (new_ptr);
 }
 
