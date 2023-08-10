5cbe90e073bf61893629745fd40844227623f88d
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-10 05:29:21 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,44 +2,44 @@
 #include <string.h>
 
 /**
- * string_nconcat - Concatenates two strings.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The number of bytes from s2 to concatenate.
- *
- * Return: A pointer to the concatenated string, or NULL on failure.
- */
+* string_nconcat - Concatenates two strings.
+* @s1: The first string.
+* @s2: The second string.
+* @n: The number of bytes from s2 to concatenate.
+*
+* Return: A pointer to the concatenated string, or NULL on failure.
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *concatenated;
-    unsigned int len1 = 0, len2 = 0, i, j;
+char *concatenated;
+unsigned int len1 = 0, len2 = 0, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-    while (s1[len1])
-        len1++;
-    while (s2[len2])
-        len2++;
+while (s1[len1])
+len1++;
+while (s2[len2])
+len2++;
 
-    if (n >= len2)
-        n = len2;
+if (n >= len2)
+n = len2;
 
-    concatenated = malloc(sizeof(char) * (len1 + n + 1));
+concatenated = malloc(sizeof(char) * (len1 + n + 1));
 
-    if (concatenated == NULL)
-        return (NULL);
+if (concatenated == NULL)
+return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concatenated[i] = s1[i];
+for (i = 0; i < len1; i++)
+concatenated[i] = s1[i];
 
-    for (j = 0; j < n; j++, i++)
-        concatenated[i] = s2[j];
+for (j = 0; j < n; j++, i++)
+concatenated[i] = s2[j];
 
-    concatenated[i] = '\0';
+concatenated[i] = '\0';
 
-    return (concatenated);
+return (concatenated);
 }
 
