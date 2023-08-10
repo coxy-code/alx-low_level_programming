e6c6b7e3e816a944b107ca7b3397b5bdbe4d9fdc
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-10 10:55:32 -0700

    malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,63 +5,14 @@
 int find_len(char *str);
 char *create_xarray(int size);
 char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
+void get_prod(char *prod, char *mult, int digit);
 void add_nums(char *final_prod, char *next_prod, int next_len);
-
-int main(int argc, char *argv[])
-{
-    char *final_prod, *next_prod;
-    int size, index, digit, zeroes = 0;
-
-    if (argc != 3)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-
-    if (*(argv[1]) == '0')
-        argv[1] = iterate_zeroes(argv[1]);
-    if (*(argv[2]) == '0')
-        argv[2] = iterate_zeroes(argv[2]);
-
-    if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
-    {
-        printf("0\n");
-        return (0);
-    }
-
-    size = find_len(argv[1]) + find_len(argv[2]);
-    final_prod = create_xarray(size + 1);
-    next_prod = create_xarray(size + 1);
-
-    for (index = find_len(argv[2]) - 1; index >= 0; index--)
-    {
-        digit = *(argv[2] + index) - '0';
-        get_prod(next_prod, argv[1], digit, zeroes++);
-        add_nums(final_prod, next_prod, size);
-    }
-
-    for (index = 0; final_prod[index]; index++)
-    {
-        if (final_prod[index] != 'x')
-            putchar(final_prod[index]);
-    }
-
-    putchar('\n');
-    free(next_prod);
-    free(final_prod);
-
-    return (0);
-}
 
 int find_len(char *str)
 {
     int len = 0;
-    while (*str)
-    {
+    while (*str++)
         len++;
-        str++;
-    }
     return (len);
 }
 
@@ -85,8 +36,7 @@ char *iterate_zeroes(char *str)
     return (str);
 }
 
-void get_prod(char *prod, char *mult, int digit, int zeroes)
-
+void get_prod(char *prod, char *mult, int digit)
 {
     int mult_len, num, tens = 0;
 
@@ -122,7 +72,6 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 void add_nums(char *final_prod, char *next_prod, int next_len)
 {
     int num, tens = 0;
-
     while (*(final_prod + 1))
         final_prod++;
     while (*(next_prod + 1))
@@ -140,5 +89,51 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
     }
     if (tens)
         *final_prod = (tens % 10) + '0';
+}
+
+int main(int argc, char *argv[])
+{
+    char *final_prod, *next_prod;
+    int size, index, digit;
+
+    if (argc != 3)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    if (*argv[1] == '0')
+        argv[1] = iterate_zeroes(argv[1]);
+    if (*argv[2] == '0')
+        argv[2] = iterate_zeroes(argv[2]);
+
+    if (*argv[1] == '\0' || *argv[2] == '\0')
+    {
+        printf("0\n");
+        return (0);
+    }
+
+    size = find_len(argv[1]) + find_len(argv[2]);
+    final_prod = create_xarray(size + 1);
+    next_prod = create_xarray(size + 1);
+
+    for (index = find_len(argv[2]) - 1; index >= 0; index--)
+    {
+        digit = *(argv[2] + index) - '0';
+        get_prod(next_prod, argv[1], digit);
+        add_nums(final_prod, next_prod, size - 1);
+    }
+
+    for (index = 0; final_prod[index]; index++)
+    {
+        if (final_prod[index] != 'x')
+            putchar(final_prod[index]);
+    }
+    putchar('\n');
+
+    free(next_prod);
+    free(final_prod);
+
+    return (0);
 }
 
