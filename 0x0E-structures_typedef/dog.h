826c020b4bd8243cede335cd669a39f981eb22eb
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-14 04:08:23 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+* struct dog - A struct representing a dog
+* @name: The name of the dog
+* @age: The age of the dog
+* @owner: The owner of the dog
+*
+* Description: This struct stores information about a dog.
+*/
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
 } dog_t;
 
 void init_dog(dog_t *d, char *name, float age, char *owner);
