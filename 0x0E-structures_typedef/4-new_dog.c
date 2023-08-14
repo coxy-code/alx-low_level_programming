5127a7804be421ff71a7b73b6c5316ea4c4b2906
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-14 05:01:22 -0700

    structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,37 +3,45 @@
 #include <string.h>
 #include "dog.h"
 
+/**
+* new_dog - Creates a new struct dog
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*
+* Return: Pointer to the newly created struct dog
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *newDog;
-    char *newName, *newOwner;
+dog_t *newDog;
+char *newName, *newOwner;
 
-    if (name == NULL || owner == NULL)
-        return NULL;
+if (name == NULL || owner == NULL)
+return (NULL);
 
-    newDog = malloc(sizeof(dog_t));
-    if (newDog == NULL)
-        return NULL;
+newDog = malloc(sizeof(dog_t));
+if (newDog == NULL)
+return (NULL);
 
-    newName = strdup(name);
-    if (newName == NULL)
-    {
-        free(newDog);
-        return NULL;
-    }
+newName = strdup(name);
+if (newName == NULL)
+{
+free(newDog);
+return (NULL);
+}
 
-    newOwner = strdup(owner);
-    if (newOwner == NULL)
-    {
-        free(newName);
-        free(newDog);
-        return NULL;
-    }
+newOwner = strdup(owner);
+if (newOwner == NULL)
+{
+free(newName);
+free(newDog);
+return (NULL);
+}
 
-    newDog->name = newName;
-    newDog->age = age;
-    newDog->owner = newOwner;
+newDog->name = newName;
+newDog->age = age;
+newDog->owner = newOwner;
 
-    return newDog;
+return (newDog);
 }
 
