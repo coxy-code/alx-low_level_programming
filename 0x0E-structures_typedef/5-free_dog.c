66b07b74b7b61c85f6219f6b796fc0fe593dd8cf
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-14 04:57:07 -0700

    structures

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,19 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+* free_dog - Frees memory allocated for a struct dog
+* @d: Pointer to the struct dog to be freed
+*
+* Return: None
+*/
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-        free(d->name);
-        free(d->owner);
-        free(d);
-    }
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
 }
 
