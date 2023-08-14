916e7278b9fc0de6c8b5f196b2479ffa1da5e502
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-14 08:24:26 -0700

    structures

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 
 /**
-* init_dog - Initializes a struct dog
+* init_dog - Initializes a struct dog with provided values
 * @d: Pointer to the struct dog to be initialized
 * @name: Name of the dog
 * @age: Age of the dog
@@ -11,8 +11,11 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d != NULL)
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
 }
 
