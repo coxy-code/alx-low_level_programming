090838275d677935cb6c3c84cdebc2799cca7de7
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-16 03:15:05 -0700

    function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,23 +1,30 @@
 #include "3-calc.h"
 
+/**
+* get_op_func - Selects the correct function to perform an operation.
+* @s: The operator passed as an argument to the program.
+*
+* Return: A pointer to the function that corresponds to the operator, or NULL
+*         if no matching operator is found.
+*/
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
 
-    while (ops[i].op != NULL)
-    {
-        if (*(ops[i].op) == *s)
-            return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+while (ops[i].op != NULL)
+{
+if (*(ops[i].op) == *s)
+return (ops[i].f);
+i++;
+}
+return (NULL);
 }
 
