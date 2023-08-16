94d1aee5f417859ce17b8cf2d96ef94b98230312
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-16 03:27:21 -0700

    function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,30 +2,37 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+* main - Entry point of the calculator program.
+* @argc: Number of command-line arguments.
+* @argv: Array of command-line arguments.
+*
+* Return: Always 0 (Success).
+*/
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operation)(int, int);
+int num1, num2, result;
+int (*operation)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 
-    operation = get_op_func(argv[2]);
-    if (operation == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+operation = get_op_func(argv[2]);
+if (operation == NULL)
+{
+printf("Error\n");
+exit(99);
+}
 
-    result = operation(num1, num2);
-    printf("%d\n", result);
+result = operation(num1, num2);
+printf("%d\n", result);
 
-    return (0);
+return (0);
 }
 
