65600bf07c42833a786a0e083751a8dda6047fb3
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-16 02:43:00 -0700

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using the provided printing function
+ * @name: name of the person
+ * @f: pointer to a function that prints a name
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name != NULL && f != NULL)
+        f(name);
+}
+
