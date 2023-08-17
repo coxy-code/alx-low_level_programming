7556782fc20df967b8286731b34fc961cbf0c107
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-17 02:57:29 -0700

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,72 @@
 #include <stdio.h>
 
 /**
+* print_char - Print a char argument.
+* @separator: The separator to use before the char.
+* @c: The char to print.
+* @first: Flag to indicate if it's the first argument.
+*/
+void print_char(char *separator, char c, int *first)
+{
+if (*first)
+*first = 0;
+else
+printf("%s", separator);
+
+printf("%c", c);
+}
+
+/**
+* print_int - Print an int argument.
+* @separator: The separator to use before the int.
+* @num: The int to print.
+* @first: Flag to indicate if it's the first argument.
+*/
+void print_int(char *separator, int num, int *first)
+{
+if (*first)
+*first = 0;
+else
+printf("%s", separator);
+printf("%d", num);
+}
+
+/**
+* print_float - Print a float argument.
+* @separator: The separator to use before the float.
+* @f: The float to print.
+* @first: Flag to indicate if it's the first argument.
+*/
+void print_float(char *separator, double f, int *first)
+{
+if (*first)
+*first = 0;
+else
+printf("%s", separator);
+
+printf("%f", f);
+}
+
+/**
+* print_string - Print a string argument.
+* @separator: The separator to use before the string.
+* @str: The string to print.
+* @first: Flag to indicate if it's the first argument.
+*/
+void print_string(char *separator, char *str, int *first)
+{
+if (*first)
+*first = 0;
+else
+printf("%s", separator);
+
+if (str)
+printf("%s", str);
+else
+printf("(nil)");
+}
+
+/**
 * print_all - Print arguments based on the format.
 * @format: A list of types of arguments passed to the function.
 * @...: The arguments to print.
@@ -11,12 +77,9 @@ void print_all(const char * const format, ...)
 {
 va_list args;
 unsigned int i = 0;
+int first = 1;
 char *separator = "";
 char current_format;
-char *str;
-int num;
-double f;
-
 va_start(args, format);
 
 while (format && format[i])
@@ -26,20 +89,16 @@ current_format = format[i];
 switch (current_format)
 {
 case 'c':
-putchar(separator[0]);
-putchar(va_arg(args, int));
+print_char(separator, va_arg(args, int), &first);
 break;
 case 'i':
-num = va_arg(args, int);
-printf("%s%d", separator, num);
+print_int(separator, va_arg(args, int), &first);
 break;
 case 'f':
-f = va_arg(args, double);
-printf("%s%f", separator, f);
+print_float(separator, va_arg(args, double), &first);
 break;
 case 's':
-separator = (str = va_arg(args, char *)) ? "" : "(nil)";
-printf("%s%s", separator, str);
+print_string(separator, va_arg(args, char *), &first);
 break;
 default:
 i++;
@@ -51,7 +110,6 @@ i++;
 }
 
 va_end(args);
-
-putchar('\n');
+printf("\n");
 }
 
