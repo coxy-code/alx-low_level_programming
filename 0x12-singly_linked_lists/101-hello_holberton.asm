47372382215e7de3400744b86869518fd1ba996a
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-24 02:23:37 -0700

    singly_linked_lists

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,27 @@
+section .data
+    hello db "Hello, Holberton", 0
+    fmt db "%s", 0
+    nl db 10, 0
+
+section .text
+    extern printf
+
+global _start
+
+_start:
+    push rsi         ; Save rsi to restore its value later
+    lea rdi, [fmt]   ; Load the format string address
+    lea rsi, [hello] ; Load the string address
+    call printf     ; Call printf with the arguments in rdi and rsi
+
+    ; Print a new line
+    lea rdi, [fmt]   ; Load the format string address
+    lea rsi, [nl]    ; Load the new line address
+    call printf     ; Call printf with the arguments in rdi and rsi
+
+    pop rsi          ; Restore rsi value
+    ; Exit
+    mov rax, 60      ; syscall number for exit
+    xor rdi, rdi     ; exit status 0
+    syscall
+
