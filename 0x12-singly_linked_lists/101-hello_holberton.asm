e1ca6ef0c59cbd7edac892a29e6fecfe546f1748
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-25 01:51:30 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,29 +1,22 @@
 section .data
-hello db "Hello, Holberton", 0
-fmt db "%s", 0
-nl db 10, 0
+    hello db "Hello, Holberton", 0
+    fmt db "%s", 0
 
 section .text
-extern printf
+    global main
+    extern printf
 
-global _start
+main:
+    push rsi             ; Save rsi to restore its value later
+    lea rdi, [fmt]       ; Load the format string address
+    lea rsi, [hello]     ; Load the string address
+    xor rax, rax        ; Clear rax before making the call
+    call printf         ; Call printf with the arguments in rdi and rsi
 
-_start:
-push rsi             ; Save rsi to restore its value later
-lea rdi, [fmt]       ; Load the format string address
-lea rsi, [hello]     ; Load the string address
-xor rax, rax        ; Clear rax before making the call
-call printf         ; Call printf with the arguments in rdi and rsi
+    pop rsi              ; Restore rsi value
 
-; Print a new line
-lea rdi, [nl]        ; Load the new line address
-xor rax, rax        ; Clear rax before making the call
-call printf         ; Call printf with the argument in rdi
-
-pop rsi              ; Restore rsi value
-
-; Exit
-mov rax, 60          ; syscall number for exit
-xor rdi, rdi         ; exit status 0
-syscall
+    ; Exit
+    mov rax, 60          ; syscall number for exit
+    xor rdi, rdi         ; exit status 0
+    syscall
 
