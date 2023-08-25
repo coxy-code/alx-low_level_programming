a12cb11f497f122e1178e9c90b01878ddc7d51be
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-25 01:44:03 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+* before_main - Function that runs before the main function.
+*
+* Description: Prints out a message before the main function is executed.
+* This function is marked with the constructor attribute to ensure it runs
+* before the main function.
+*/
 void __attribute__((constructor)) before_main()
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
 
