ed8058a6a8660cbea424581f1858a5ed645564b4
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-25 01:12:32 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,35 +3,35 @@
 #include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: A pointer to a pointer to the head node of the list.
- * @str: The string to be added to the new node.
- *
- * Return: The address of the new element, or NULL if it failed.
- */
+* add_node - Adds a new node at the beginning of a list_t list.
+* @head: A pointer to a pointer to the head node of the list.
+* @str: The string to be added to the new node.
+*
+* Return: The address of the new element, or NULL if it failed.
+*/
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    unsigned int len = 0;
+list_t *new_node;
+unsigned int len = 0;
 
-    while (str && str[len])
-        len++;
+while (str && str[len])
+len++;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-    new_node->len = len;
-    new_node->next = *head;
-    *head = new_node;
+new_node->len = len;
+new_node->next = *head;
+*head = new_node;
 
-    return (new_node);
+return (new_node);
 }
 
