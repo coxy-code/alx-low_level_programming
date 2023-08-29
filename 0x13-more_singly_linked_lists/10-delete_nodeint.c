49709ea10910bfe1425b9193731af40abfa57388
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-29 02:03:20 -0700

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* delete_nodeint_at_index - Deletes the node at a given index of a listint_t list.
+* @head: Double pointer to the head of the list.
+* @index: Index of the node to be deleted.
+*
+* Return: 1 if succeeded, -1 if it failed.
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *current, *temp;
+unsigned int i;
+
+if (head == NULL || *head == NULL)
+return (-1);
+
+if (index == 0)
+{
+temp = *head;
+*head = (*head)->next;
+free(temp);
+return (1);
+}
+
+current = *head;
+for (i = 0; current != NULL && i < index - 1; i++)
+current = current->next;
+
+if (current == NULL || current->next == NULL)
+return (-1);
+
+temp = current->next;
+current->next = temp->next;
+free(temp);
+
+return (1);
+}
+
