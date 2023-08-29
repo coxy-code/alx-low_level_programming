5c7eae07d2733fcec41877ecb700d7746efa3924
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-29 02:30:54 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,40 +1,42 @@
-#include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
 
 /**
 * print_listint_safe - Prints a listint_t linked list.
-* @head: Pointer to the beginning of the list.
+* @head: Pointer to the head of the list.
+*
 * Return: The number of nodes in the list.
 */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t count = 0;
 const listint_t *slow, *fast;
+size_t count = 0;
 
 slow = head;
 fast = head;
 
-while (fast != NULL && fast->next != NULL)
+while (slow && fast && fast->next)
 {
 printf("[%p] %d\n", (void *)slow, slow->n);
 slow = slow->next;
 fast = fast->next->next;
+
+count++;
 if (slow == fast)
 {
-count++;
-printf("[%p] %d\n", (void *)slow, slow->n);
-break;
+printf("-> [%p] %d\n", (void *)slow, slow->n);
+exit(98); // Exit with status 98 if loop detected
 }
 }
 
-while (slow != fast)
+while (slow)
 {
 printf("[%p] %d\n", (void *)slow, slow->n);
 slow = slow->next;
 count++;
 }
 
-return count;
+return (count);
 }
 
