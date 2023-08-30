7f2ad72a170e57f1f512723857540bf96d1be0d2
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-30 13:06:40 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -29,7 +29,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 
 listint_t *add_nodeint(listint_t **head, int n);
-void free_listint_safe(listint_t **h);
+size_t free_listint_safe(listint_t **h); // Update this declaration
 listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H */
