4418a2f3127aedf0b37d5cc862c726bb22b52de6
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-30 13:03:03 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,30 @@
-#include <stdlib.h>  
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-* free_listint_safe - Frees a listint_t list, handling potential loops
-* @h: A pointer to a pointer to the head node of the linked list
-* Return: The size of the list that was freed
-*/
+ * free_listint_safe - Frees a listint_t list, handling potential loops
+ * @h: A pointer to a pointer to the head node of the linked list
+ * Return: The size of the list that was freed
+ */
 size_t free_listint_safe(listint_t **h)
 {
-size_t count = 0;
-listint_t *current = *h, *next;
+    size_t count = 0;
+    listint_t *current = *h, *next;
 
-while (current)
-{
-next = current->next;
-free(current);
-count++;
+    while (current)
+    {
+        next = current->next;
+        free(current);
+        count++;
 
-/* Detect if the next node has been visited before */
-if ((size_t)next <= (size_t)current)
-break;
+        /* Detect if the next node has been visited before */
+        if ((size_t)next <= (size_t)current)
+            break;
 
-current = next;
-}
+        current = next;
+    }
 
-*h = NULL;
-return (count);
+    *h = NULL;
+    return count;
 }
 
