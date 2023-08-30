e8c5ea5ace9cd7ab95043dca334a3a1360eeab2c
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-30 13:00:21 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,37 +10,27 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/* Function prototype for print_listint */
-size_t print_listint(const listint_t *h);
-
 /* Function prototypes */
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 
-/* Function prototypes */
 listint_t *add_nodeint(listint_t **head, const int n);
-
 listint_t *add_nodeint_end(listint_t **head, const int n);
 
-/* Function prototypes */
-void free_listint(listint_t *head);size_t print_listint(const listint_t *h);
+void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
-/* Function prototypes */
 int pop_listint(listint_t **head);
 
-/* Function prototypes */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+
 listint_t *add_nodeint(listint_t **head, int n);
-/* Function prototypes */
 void free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-
-
 #endif /* LISTS_H */
-
 
