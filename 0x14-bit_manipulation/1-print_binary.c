e2e2b8ec620ca5f46dd2685968195b04f7e3ca99
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-31 10:48:14 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,15 @@
 #include "main.h"
+#include <unistd.h> // Include for the write function
+
+/**
+* _putchar - Writes a character to stdout.
+* @c: The character to print.
+* Return: 1 on success, -1 on error.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
 
 /**
 * print_binary - Prints the binary representation of a number.
@@ -7,9 +18,9 @@
 void print_binary(unsigned long int n)
 {
 if (n > 1)
-print_binary(n >> 1); /* Right-shift n by 1 and call the function recursively*/
+print_binary(n >> 1); /* Right-shift n by 1 and call the function recursively */
 
-_putchar((n & 1) + '0'); /* Print the least significant bit*/
+_putchar((n & 1) + '0'); /* Print the least significant bit */
 
 return;
 }
