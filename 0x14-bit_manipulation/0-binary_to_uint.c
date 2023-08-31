11259b451389c344318ffe7711e2534c27795a01
coxy-code <cosmaskimutai.ck@gmail.com>
2023-08-31 09:57:45 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* binary_to_uint - Converts a binary number to an unsigned int.
+* @b: A pointer to a string of 0 and 1 chars.
+* Return: The converted number, or 0 if there's an invalid char or b is NULL.
+*/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int result = 0;
+unsigned int bit;
+
+if (b == NULL)
+return (0);
+
+for (; *b; b++)
+{
+if (*b != '0' && *b != '1')
+return (0);
+
+bit = *b - '0'; // Convert char '0' or '1' to integer 0 or 1
+result = (result * 2) + bit;
+}
+return (result);
+}
+
+int main(void)
+{
+unsigned int n;
+
+n = binary_to_uint("1");
+printf("%u\n", n);
+n = binary_to_uint("101");
+printf("%u\n", n);
+n = binary_to_uint("1e01");
+printf("%u\n", n);
+n = binary_to_uint("1100010");
+printf("%u\n", n);
+n = binary_to_uint("0000000000000000000110010010");
+printf("%u\n", n);
+return (0);
+}
+
