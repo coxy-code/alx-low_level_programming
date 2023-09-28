47b20218599806038f736f72ccb1e04150443f0d
coxy-code <cosmaskimutai.ck@gmail.com>
2023-09-28 03:42:32 -0700

    bit_manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,27 +1,18 @@
 #include "main.h"
 
 /**
-* clear_bit - Sets the value of a bit to 0 at a given index.
-* @n: A pointer to the number whose bit you want to clear.
-* @index: The index of the bit to clear, starting from 0 (LSB).
+* clear_bit - sets the value of a bit to 0 at a given index
+* @n: pointer to the number
+* @index: the index of the bit to clear
 *
-* Return: 1 if it worked, or -1 if an error occurred.
+* Return: 1 if it worked, or -1 if an error occurred
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-/* Check if the index is out of bounds */
 if (index >= (sizeof(unsigned long int) * 8))
-return (-1);
+return (-1); /* Index out of range */
 
-/* Use a bitmask with a 1 at the desired index and 0s elsewhere */
-unsigned long int mask = 1UL << index;
-
-/* Use bitwise NOT to create a mask with 1s everywhere except the index */
-mask = ~mask;
-
-/* Use bitwise AND to clear the bit at the specified index */
-*n = *n & mask;
-
+*n = *n & ~(1 << index);
 return (1);
 }
 
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,6 @@ int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int set_bit(unsigned long int *n, unsigned int index);
-
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif /* MAIN_H */
 
