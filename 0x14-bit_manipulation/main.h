6b63b4b8e72a99455804059b1dd82ccfb3203457
coxy-code <cosmaskimutai.ck@gmail.com>
2023-09-28 03:32:32 -0700

    bit_manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,12 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int _putchar(char c);
-
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
+void print_binary(unsigned long int n);
+unsigned int binary_to_uint(const char *b);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif /* MAIN_H */
 
