ce3e5bd0c51c2e385c070771e725e134b4e0ec4c
coxy-code <cosmaskimutai.ck@gmail.com>
2023-09-28 03:22:16 -0700

    bit_manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* get_bit - gets the value of a bit at a given index
+* @n: the number to extract the bit from
+* @index: the index of the bit to get
+*
+* Return: the value of the bit at the index or -1 if an error occurred
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index >= (sizeof(unsigned long int) * 8))
+return (-1); /* Index out of range */
+
+return ((n >> index) & 1);
+}
+
